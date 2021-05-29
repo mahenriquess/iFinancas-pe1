37ec1ce9a2e45e8860738fa7d59c6a64637b343f
Marcelo Henrique da Silva <mahenriquess@outlook.com>
2021-05-29 12:08:13 -0300

    Adicionando configurações do firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDVvW0c_VksxfikIARoF9Du50Mqb130qM4',
+    authDomain: 'controle-mahenriquess.firebaseapp.com',
+    projectId: 'controle-mahenriquess',
+    storageBucket: 'controle-mahenriquess.appspot.com',
+    messagingSenderId: '302665713874',
+    appId: '1:302665713874:web:b139e9806cdeb2353c050b',
+    measurementId: 'G-282SNXG5C9'
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDVvW0c_VksxfikIARoF9Du50Mqb130qM4',
+    authDomain: 'controle-mahenriquess.firebaseapp.com',
+    projectId: 'controle-mahenriquess',
+    storageBucket: 'controle-mahenriquess.appspot.com',
+    messagingSenderId: '302665713874',
+    appId: '1:302665713874:web:b139e9806cdeb2353c050b',
+    measurementId: 'G-282SNXG5C9'
+  },
 };
 
 /*
